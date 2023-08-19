c0f9005a581b3be5daece187bfee546edfc05dce
Pawan Jindal <45796558+darena-pjindal@users.noreply.github.com>
2023-08-19 06:30:15 -0500

    Initial commit

---
+++ MVCToBlazor/MVCToBlazor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCToBlazor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
